46f8066424adcee3a88446075260c755e9010622
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-02-07 15:45:55 +0900

    충돌체크

--- Assets/2.Scripts/RubyController.cs
+++ Assets/2.Scripts/RubyController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
     public float moveSpeed = 4.0f;
-    void Start()
+    public int maxHealth = 5;
+    public int health { get { return currentHealth;}}
+    private int currentHealth;
+    private Rigidbody2D rb2d;
+
+    private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        currentHealth = 1; //maxHealth;
     }
 
-    void Update()
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -24,5 +29,13 @@ public class RubyController : MonoBehaviour
         position.x += moveSpeed * horizontal * Time.deltaTime;
         position.y += moveSpeed * vertical * Time.deltaTime;
         rb2d.MovePosition(position);
+
+        
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        //Debug.Log(currentHealth + "/" + maxHealth);
+        Debug.Log($"{currentHealth}/{maxHealth}");
     }
 }
